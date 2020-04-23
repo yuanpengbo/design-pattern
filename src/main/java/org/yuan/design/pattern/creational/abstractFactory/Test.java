6c4dd38e54577da73adf294a8c2ba0fa8fe48cea
yuanpengbo <qqppu_001@126.com>
2020-04-24 00:32:20 +0800

    Clean up excess code

--- src/main/java/org/yuan/design/pattern/creational/abstractFactory/Test.java
+++ src/main/java/org/yuan/design/pattern/creational/abstractFactory/Test.java
@@ -1,10 +1,5 @@
 package org.yuan.design.pattern.creational.abstractFactory;
 
-import org.yuan.design.pattern.creational.factorymethod.JavaVideoFactory;
-import org.yuan.design.pattern.creational.factorymethod.PythonVideoFactory;
-import org.yuan.design.pattern.creational.factorymethod.Video;
-import org.yuan.design.pattern.creational.factorymethod.VideoFactory;
-
 public class Test {
     public static void main(String[] args) {
        CourseFactory courseFactory = new JavaCourseFactory();
--- src/main/java/org/yuan/design/pattern/creational/simplefactory/Test.java
+++ src/main/java/org/yuan/design/pattern/creational/simplefactory/Test.java
@@ -1,7 +1,5 @@
 package org.yuan.design.pattern.creational.simplefactory;
 
-import java.util.Calendar;
-
 public class Test {
     public static void main(String[] args) {
         VideoFactory videoFactory = new VideoFactory();
