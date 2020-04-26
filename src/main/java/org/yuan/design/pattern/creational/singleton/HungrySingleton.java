67d1789568adea6d1533210adde6f19750f5b0d3
yuanpengbo <qqppu_001@126.com>
2020-04-27 00:17:02 +0800

    singleton

--- src/main/java/org/yuan/design/pattern/creational/singleton/HungrySingleton.java
+++ src/main/java/org/yuan/design/pattern/creational/singleton/HungrySingleton.java
@@ -17,5 +17,5 @@ public class HungrySingleton {
     private HungrySingleton() {
     }
 
-    
+
 }
---
+++ src/main/java/org/yuan/design/pattern/creational/singleton/Test.java
@@ -0,0 +1,28 @@
+package org.yuan.design.pattern.creational.singleton;
+
+import java.util.Date;
+
+public class Test {
+
+    public static void main(String[] args) {
+//        LazySingleton lazySingleton = LazySingleton.getInstance();
+//        System.out.println(lazySingleton);
+
+
+//        Thread t1 = new Thread(new T());
+//        Thread t2 = new Thread(new T());
+//        t1.start();
+//        t2.start();
+//        System.out.println("end");
+
+//        Date date1 = new Date();
+//        Date date2 = new Date();
+//        ContainerSingelton.putInstance(Date.class,date1);
+//        ContainerSingelton.putInstance(Date.class,date2);
+//
+//        System.out.println(date1==ContainerSingelton.getInstance(Date.class));
+//        System.out.println(date2==ContainerSingelton.getInstance(Date.class));
+//
+//        EnumSingleton.INTERFACE.print();
+    }
+}
